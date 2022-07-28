3a6c9b393c9ccf0ab52ca69e3b9f45824fcb738c
tarepan <tarepan5884@gmail.com>
2022-07-28 06:39:50 +0000

    Add tree sampling

---
+++ lpcnet/networks/tree_sampling.py
@@ -0,0 +1,63 @@
+"""Tree sampling"""
+
+import math
+
+from torch import Tensor, cat, repeat_interleave # pylint: disable=no-name-in-module
+
+
+def _cprobs_to_cdist(c_probs: Tensor, num_joint_bits: int, idx_layer: int, ndim_cprob_b: int, ndim_cprob_t: int) -> Tensor:
+    """Convert conditional probabilities into conditional probability distributions with inteleave for joint probability distribution.
+
+    Args:
+        c_probs :: (B, T, Cond=2**(k-1)) - B(L_k=1|L_<k=cond) conditional probabilities of all conditions of layer k
+        num_joint_bits                   - The number of joint distribution's bits (q)
+        idx_layer                        - Index number 'k' of the `c_probs` layer
+        ndim_cprob_b                     - The size of c_prob's batch dimension (batch size)
+        ndim_cprob_t                     - The size of c_prob's time dimension (time length)
+
+    Returns:
+        :: (B, T, Dist=2**q) - Interleaved B(L_k=1|L_<k=cond) conditional probabilities distribution of all conditions of layer k
+    """
+
+    # Conditional Prob.s to Conditional Prob. distributions
+    #   (B, T, Cond=2**(k-1)) -> (B, T, Cond=2**(k-1), 1) -> (B, T_s, Cond=2**(k-1), Dist=2)
+    c_probs = c_probs.unsqueeze(3)
+    #                    B(Lk=0|.), B(Lk=1|.)
+    c_dists = cat((1.0-c_probs, c_probs), dim=3)
+
+    # Interleave for joint distribution
+    # x2 conditions per bit layer (2**(q-k))
+    num_repeat = 2 ** (num_joint_bits - idx_layer)
+    ## (B, T, Cond=2**(k-1), Dist=2) -> (B, T, cDist=2**k)
+    c_dists = c_dists.reshape((ndim_cprob_b, ndim_cprob_t, -1))
+    ## (B, T, cDist=2**k) -> (B, T, cDist=2**q)
+    c_dists_interleaved = repeat_interleave(c_dists, num_repeat, dim=2)
+
+    return c_dists_interleaved
+
+
+def tree_to_pdf(cprobs: Tensor) -> Tensor:
+    """Convert Hierarchical conditional bit probabilities to the joint probability.
+
+         B(Lk=1|L<k)     B(Lk=0|L<k=Bs<k)/B(Lk=1|L<k=Bs<k)      P(level)
+    L1        0.6                     0.4/0.6               0.4  0.6  0.4  0.6
+            /     ╲                                          x    x    x    x
+    L2    0.1     0.7      =>    0.9/0.1   0.3/0.7    =>    0.9  0.1  0.3  0.7
+         .   .   .   .
+    P(s).36/.04/.18/.42                                     0.36 0.04 0.18 0.42
+
+    Args:
+        cprobs         :: (B, T, Prob=2**q) - Hierarchical conditional bit probabilities
+    Returns
+                       :: (B, T, Dist=2**q) - Joint probability distribution of all bits == PD of level
+    """
+
+    ndim_b = cprobs.shape[0]
+    ndim_t = cprobs.shape[1]
+    num_joint_bits = round(math.log2(cprobs.shape[2]))
+
+    # Joint Probability Distribution == Π P(L_k|L_<k)
+    prob_dist =                 _cprobs_to_cdist(cprobs[:,:, 2**(1-1): 2**(1)],                num_joint_bits,         1, ndim_b, ndim_t)
+    for idx_layer in range(2, num_joint_bits + 1):
+        prob_dist = prob_dist * _cprobs_to_cdist(cprobs[:,:, 2**(idx_layer-1) : 2**idx_layer], num_joint_bits, idx_layer, ndim_b, ndim_t)
+    return prob_dist
---
+++ lpcnet/networks/tree_sampling_test.py
@@ -0,0 +1,85 @@
+"""Test tree sampling"""
+
+from torch import Tensor, tensor, float64, equal # pylint: disable=no-name-in-module
+
+from .tree_sampling import _cprobs_to_cdist, tree_to_pdf # pyright: ignore [reportPrivateUsage]
+
+
+def test_cprobs_to_cdist():
+    """Test `_cprobs_to_cdist` function."""
+
+    batch_size = 2
+    t_sample = 3
+    bit_depth = 3
+
+    # P(bit_k|bit_<k) :: (Batch, Time, Bit)
+    bit_cond_probs: Tensor = tensor([
+        [ # batch_0
+            [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,], # t_0
+            [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,], # t_1
+            [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,], # t_2
+        #   | - | L1 |    L2   |        L3         |
+        ],
+        [ # batch_1
+            [1.0, 1.0, 1.0, 1.0, 0.1, 0.2, 0.3, 0.4,], # t_0
+            [1.0, 1.0, 1.0, 1.0, 0.5, 0.6, 0.7, 0.8,], # t_1
+            [1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0,], # t_2
+        #   | - | L1 |    L2   |        L3         |
+        ],
+    ], dtype=float64)
+
+    # (B=2, T_s=3, Prob=4)
+    layer = 3
+    assert 2**(layer-1) == 4, "Prerequisites"
+    assert 2**layer == 8, "Prerequisites"
+    l3: Tensor = bit_cond_probs[:, :, 2**(layer-1) : 2**layer]
+
+    l3_gt: Tensor = tensor([
+        [ # batch_0
+            [1.0-1.0, 1.0, 1.0-1.0, 1.0, 1.0-1.0, 1.0, 1.0-1.0, 1.0,], # t_0
+            [1.0-1.0, 1.0, 1.0-1.0, 1.0, 1.0-1.0, 1.0, 1.0-1.0, 1.0,], # t_1
+            [1.0-1.0, 1.0, 1.0-1.0, 1.0, 1.0-1.0, 1.0, 1.0-1.0, 1.0,], # t_2
+        #   |   L      H      L      H      L      H      L      H  |
+        ],
+        [ # batch_1
+            [1.0-0.1, 0.1, 1.0-0.2, 0.2, 1.0-0.3, 0.3, 1.0-0.4, 0.4,], # t_0
+            [1.0-0.5, 0.5, 1.0-0.6, 0.6, 1.0-0.7, 0.7, 1.0-0.8, 0.8,], # t_1
+            [1.0-0.0, 0.0, 1.0-0.0, 0.0, 1.0-0.0, 0.0, 1.0-0.0, 0.0,], # t_2
+        #   |   L      H      L      H      L      H      L      H  |
+        ],
+    ], dtype=float64)
+
+    l3_cond_probs = _cprobs_to_cdist(l3, bit_depth, layer, batch_size, t_sample)
+
+    assert equal(l3_cond_probs, l3_gt)
+
+
+def test_tree_to_pdf():
+    """Test `tree_to_pdf` function."""
+
+    # P(bit_k|bit_<k) :: (B=1, T_s=1, Cond=2**3)
+    bit_cond_probs_all: Tensor = tensor([
+        [ # batch_1
+            [1.0, 0.3, 0.4, 0.5, 0.1, 0.2, 0.3, 0.4,], # t_0
+        #   | - | L1 |    L2   |        L3         |
+        ],
+    ], dtype=float64)
+
+    # P(level) :: (B=1, T_s=1, Dist=2**3)
+    joint_dist_gt: Tensor = tensor([
+        [ # batch_0
+            [ # time_0
+                (1.0-0.3) * (1.0-0.4) * (1.0-0.1), # 000
+                (1.0-0.3) * (1.0-0.4) *      0.1 , # 001
+                (1.0-0.3) *      0.4  * (1.0-0.2), # 010
+                (1.0-0.3) *      0.4  *      0.2 , # 011
+                     0.3  * (1.0-0.5) * (1.0-0.3), # 100
+                     0.3  * (1.0-0.5) *      0.3 , # 101
+                     0.3  *      0.5  * (1.0-0.4), # 110
+                     0.3  *      0.5  *      0.4 , # 111
+        ],
+    ]], dtype=float64)
+
+    joint_dist_estim = tree_to_pdf(bit_cond_probs_all)
+
+    assert equal(joint_dist_estim, joint_dist_gt)
