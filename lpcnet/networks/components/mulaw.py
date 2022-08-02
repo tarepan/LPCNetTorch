f247066f30b05ebc9be694ee7edd4d3f959fb9bb
tarepan <tarepan5884@gmail.com>
2022-08-02 12:36:26 +0000

    Add discrete μ-law

--- lpcnet/networks/components/mulaw.py
+++ lpcnet/networks/components/mulaw.py
@@ -1,29 +1,48 @@
 """MuLaw conversion"""
 
-from torch import Tensor, tensor, float32, abs, clamp, log, sgn # pylint: disable=no-name-in-module,redefined-builtin
+from torch import Tensor, FloatTensor, LongTensor, tensor, float32, int64, abs, clamp, log, round, sgn # pylint: disable=no-name-in-module,redefined-builtin
 
 
-def lin2mlaw(linear_int: Tensor) -> Tensor:
-    """Convert linear int16 signals into μ-law int8 scale (just scale, not discretized).
+# lin/mlaw     - 'Linear' or 'μ-law' scale
+# (non)/s16/u8 - Range [-1, 1] | [-32768, +32767] | [0, 255]
+# (non)/pcm    - Continuous | Discretized
+
+def lin2mlaw(linear_s16: Tensor) -> FloatTensor:
+    """Encode linear sint16-scale signals into μ-law uint8-scale (just scale, not discretized).
 
     I/O is FloatTensor, so differential.
 
     Args:
-        linear_int :: (...) - Linear PCM int16-scale [-32768, +32767] signal
+        linear_s16 :: (...) - Linear sint16-scale [-32768., +32767.] signal
     Returns:
-        mulaw_int  :: (...) - μ-law int8-scale [-256, +255] signal
+        mlaw_u8    :: (...) - μ-law uint8-scale [0., +255.] continuous signal
     """
 
-    scale = 32768.0
-    mu = tensor(255.0, dtype=float32) # pylint: disable=invalid-name
+    scale_s16 = 32768.0
+    mu8bit = tensor(255.0, dtype=float32) # pylint: disable=invalid-name
 
     # Scaling :: [-32768, +32767] -> [-1, +1)
-    linear = linear_int / scale
+    linear = linear_s16 / scale_s16
 
     # linear-to-μlaw
-    ulaw = sgn(linear) * log(1. + mu * abs(linear))/ log(1. + mu)
+    mlaw = sgn(linear) * log(1. + mu8bit * abs(linear))/ log(1. + mu8bit)
 
     # Rescaling :: [-1, +1) -> [0, +2) -> [0, 255]
-    ulaw_int = clamp(128 * (ulaw + 1), min=0, max=255)
+    mlaw_u8 = clamp(128 * (mlaw + 1), min=0, max=255)
 
-    return ulaw_int
+    return mlaw_u8 # type: ignore
+
+
+def lin2mlawpcm(linear_s16: Tensor) -> LongTensor:
+    """Convert linear int16-scale signals into μ-law 8bit PCM (discrete) signals.
+
+    Signal is discretized, so un-differential.
+
+    Args:
+        linear_s16  :: (...) - Linear sint16-scale [-32768., +32767.] signal
+    Returns:
+        mulaw_u8pcm :: (...) - μ-law uint8-scale [0, +255] PCM (discrete) signal
+    """
+    mlaw_u8 = lin2mlaw(linear_s16)
+    mlaw_u8pcm = round(mlaw_u8).to(int64)
+    return mlaw_u8pcm # type: ignore
--- lpcnet/networks/components/mulaw_test.py
+++ lpcnet/networks/components/mulaw_test.py
@@ -1,8 +1,8 @@
 """Test μ-law conversion"""
 
-from torch import tensor, float32, equal # pylint: disable=no-name-in-module,redefined-builtin
+from torch import tensor, float32, int64, equal # pylint: disable=no-name-in-module,redefined-builtin
 
-from .mulaw import lin2mlaw
+from .mulaw import lin2mlaw, lin2mlawpcm
 
 
 def test_lin2mlaw():
@@ -23,3 +23,23 @@ def test_lin2mlaw():
     assert equal(lin2mlaw(i_big)   , i_big_gt)
     assert equal(lin2mlaw(i_max)   , i_max_gt)
     assert equal(lin2mlaw(i_toomax), i_toomax_gt)
+
+
+def test_lin2mlawpcm():
+    """Test `lin2mlawpcm`."""
+
+    i_toomin, i_toomin_gt = tensor(-32768.1, dtype=float32), tensor(  0, dtype=int64)
+    i_min   , i_min_gt    = tensor(-32768. , dtype=float32), tensor(  0, dtype=int64)
+    i_small , i_small_gt  = tensor(-32767. , dtype=float32), tensor(  0, dtype=int64)
+    i_zero  , i_zero_gt   = tensor(     0. , dtype=float32), tensor(128, dtype=int64)
+    i_big   , i_big_gt    = tensor(+31266. , dtype=float32), tensor(255, dtype=int64)
+    i_max   , i_max_gt    = tensor(+32767. , dtype=float32), tensor(255, dtype=int64)
+    i_toomax, i_toomax_gt = tensor(+32767.1, dtype=float32), tensor(255, dtype=int64)
+
+    assert equal(lin2mlawpcm(i_toomin), i_toomin_gt)
+    assert equal(lin2mlawpcm(i_min)   , i_min_gt)
+    assert equal(lin2mlawpcm(i_small) , i_small_gt)
+    assert equal(lin2mlawpcm(i_zero)  , i_zero_gt)
+    assert equal(lin2mlawpcm(i_big)   , i_big_gt)
+    assert equal(lin2mlawpcm(i_max)   , i_max_gt)
+    assert equal(lin2mlawpcm(i_toomax), i_toomax_gt)
