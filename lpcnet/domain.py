7fb1fa5945486452d3c5673816c3c701686bf999
tarepan <tarepan5884@gmail.com>
2022-08-05 11:15:31 +0000

    Add domain notation

--- lpcnet/domain.py
+++ lpcnet/domain.py
@@ -14,10 +14,10 @@ from torch import Tensor # pyright: ignore [reportUnknownVariableType] ; because
 #     StSeriesCleanBatched  :: (B, T=spl_cnk)            - Sample series (linear_s16pcm waveform) w/o noise
 
 FeatSeriesBatched     = Tensor # FloatTensor
-PitchSeriesBatched    = Tensor # LongTensor
+PitchSeriesBatched    = Tensor # IntTensor
 LPCoeffSeriesBatched  = Tensor # FloatTensor
-St1SeriesNoisyBatched = Tensor # LongTensor
-StSeriesCleanBatched  = Tensor # LongTensor
+St1SeriesNoisyBatched = Tensor # IntTensor (linear_s16pcm needs only 16bit/ShortTensor, but nn.Embedding needs 32bit or 64bit)
+StSeriesCleanBatched  = Tensor # IntTensor (linear_s16pcm needs only 16bit/ShortTensor, but nn.Embedding needs 32bit or 64bit)
 
 ## the batch
 FPitchCoeffSt1nStcBatch = Tuple[FeatSeriesBatched, PitchSeriesBatched, LPCoeffSeriesBatched, St1SeriesNoisyBatched, StSeriesCleanBatched]
