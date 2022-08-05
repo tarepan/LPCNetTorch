f572c9d3a4468096cdb47e120f2cec551f830a08
tarepan <tarepan5884@gmail.com>
2022-08-05 11:15:06 +0000

    Add mulaw-to-linear and discritization

--- lpcnet/networks/components/mulaw.py
+++ lpcnet/networks/components/mulaw.py
@@ -1,11 +1,24 @@
 """MuLaw conversion"""
 
-from torch import Tensor, FloatTensor, LongTensor, tensor, float32, int64, abs, clamp, log, round, sgn # pylint: disable=no-name-in-module,redefined-builtin
+from torch import Tensor, FloatTensor, IntTensor, LongTensor, tensor, float32, int32, int64, abs, clamp, log, pow, round, sgn # pylint: disable=no-name-in-module,redefined-builtin
 
 
 # lin/mlaw     - 'Linear' or 'μ-law' scale
 # (non)/s16/u8 - Range [-1, 1] | [-32768, +32767] | [0, 255]
 # (non)/pcm    - Continuous | Discretized
+
+
+def linear_s16pcm(linear_s16: Tensor) -> IntTensor:
+    """Discretize linear_s16 continuous signals into linear_s16pcm discrete signals.
+
+    Args:
+        linear_s16 :: (...) - Linear sint16-scale signal
+    Returns:
+        linear_s16_pcm :: (...) - Linear sint16 PCM signal (not ShortTensor but IntTensor because of nn.Embedding input limitation)
+    """
+    linear_s16_pcm = clamp(round(linear_s16), min=-32768, max=+32767)
+    return linear_s16_pcm.to(int32) # type: ignore ; because of PyTorch cast
+
 
 def lin2mlaw(linear_s16: Tensor) -> FloatTensor:
     """Encode linear sint16-scale signals into μ-law uint8-scale (just scale, not discretized).
@@ -46,3 +59,27 @@ def lin2mlawpcm(linear_s16: Tensor) -> LongTensor:
     mlaw_u8 = lin2mlaw(linear_s16)
     mlaw_u8pcm = round(mlaw_u8).to(int64)
     return mlaw_u8pcm # type: ignore
+
+
+def mlaw2lin(mlaw_u8: Tensor) -> Tensor:
+    """Decode μ-law uint8-scale signals into linear sint16-scale.
+
+    Args:
+        mlaw_u8    :: (...) - μ-law uint8-scale [0., +255.] signal
+    Returns:
+        linear_s16 :: (...) - Linear sint16-scale [-32768., +32767.] signal
+    """
+
+    scale_s16 = 32768.0
+    mu8bit = tensor(255.0, dtype=float32) # pylint: disable=invalid-name
+
+    # Scaling :: [0, +255] -> [-128, +127] -> [-1, +1)
+    mlaw = (mlaw_u8 - 128.) / 128.
+
+    # μlaw-to-linear
+    linear = sgn(mlaw) * (pow(1. + mu8bit, abs(mlaw)) - 1) / mu8bit
+
+    # Rescaling :: [-1, +1) -> [-32768., +32767.]
+    mlaw_u8 = clamp(scale_s16 * linear, min=-32768., max=+32767.)
+
+    return mlaw_u8
--- lpcnet/networks/components/mulaw_test.py
+++ lpcnet/networks/components/mulaw_test.py
@@ -1,8 +1,32 @@
 """Test μ-law conversion"""
 
-from torch import tensor, float32, int64, equal # pylint: disable=no-name-in-module,redefined-builtin
+from torch import tensor, float32, int32, int64, equal # pylint: disable=no-name-in-module,redefined-builtin
 
-from .mulaw import lin2mlaw, lin2mlawpcm
+from .mulaw import lin2mlaw, lin2mlawpcm, linear_s16pcm, mlaw2lin
+
+
+def test_linear_pcm():
+    """Test `linear_pcm`."""
+
+    i_toomin, i_toomin_gt = tensor(-32768.1, dtype=float32), tensor(-32768, dtype=int32)
+    i_min   , i_min_gt    = tensor(-32768. , dtype=float32), tensor(-32768, dtype=int32)
+    i_small , i_small_gt  = tensor(-32767. , dtype=float32), tensor(-32767, dtype=int32)
+    i_zero  , i_zero_gt   = tensor(     0. , dtype=float32), tensor(     0, dtype=int32)
+    i_rounds, i_rounds_gt = tensor(     0.1, dtype=float32), tensor(     0, dtype=int32)
+    i_roundl, i_roundl_gt = tensor(     0.7, dtype=float32), tensor(     1, dtype=int32)
+    i_big   , i_big_gt    = tensor(+31266. , dtype=float32), tensor(+31266, dtype=int32)
+    i_max   , i_max_gt    = tensor(+32767. , dtype=float32), tensor(+32767, dtype=int32)
+    i_toomax, i_toomax_gt = tensor(+32767.1, dtype=float32), tensor(+32767, dtype=int32)
+
+    assert equal(linear_s16pcm(i_toomin), i_toomin_gt)
+    assert equal(linear_s16pcm(i_min)   , i_min_gt)
+    assert equal(linear_s16pcm(i_small) , i_small_gt)
+    assert equal(linear_s16pcm(i_zero)  , i_zero_gt)
+    assert equal(linear_s16pcm(i_rounds), i_rounds_gt)
+    assert equal(linear_s16pcm(i_roundl), i_roundl_gt)
+    assert equal(linear_s16pcm(i_big)   , i_big_gt)
+    assert equal(linear_s16pcm(i_max)   , i_max_gt)
+    assert equal(linear_s16pcm(i_toomax), i_toomax_gt)
 
 
 def test_lin2mlaw():
@@ -43,3 +67,32 @@ def test_lin2mlawpcm():
     assert equal(lin2mlawpcm(i_big)   , i_big_gt)
     assert equal(lin2mlawpcm(i_max)   , i_max_gt)
     assert equal(lin2mlawpcm(i_toomax), i_toomax_gt)
+
+
+def test_mlaw2lin():
+    """Test `mlaw2lin`."""
+
+    i_toomin , i_toomin_gt  = tensor( -0.1     , dtype=float32), tensor(-32768.     , dtype=float32)
+    i_min    , i_min_gt     = tensor(  0.      , dtype=float32), tensor(-32768.     , dtype=float32)
+    i_zero   , i_zero_gt    = tensor(128.      , dtype=float32), tensor(     0.     , dtype=float32)
+    i_max    , i_max_gt     = tensor(255.      , dtype=float32), tensor(+31373.2969 , dtype=float32)
+    i_toomax , i_toomax_gt  = tensor(256.      , dtype=float32), tensor(+32767.     , dtype=float32)
+
+    assert equal(mlaw2lin(i_toomin),  i_toomin_gt)
+    assert equal(mlaw2lin(i_min)   ,  i_min_gt)
+    assert equal(mlaw2lin(i_zero)  ,  i_zero_gt)
+    assert equal(mlaw2lin(i_max)   ,  i_max_gt)
+    assert equal(mlaw2lin(i_toomax),  i_toomax_gt)
+
+
+def test_mlaw_conversion():
+    """Test linear <-> mulaw."""
+
+    i_min            = tensor(-32768., dtype=float32)
+    i_zero           = tensor(     0., dtype=float32)
+    # 255] clipping prevent '+32767' int16 max
+    i_max,  i_max_gt = tensor(+32767., dtype=float32), tensor(+31373.2969 , dtype=float32)
+
+    assert equal(mlaw2lin(lin2mlaw(i_min)),  i_min)
+    assert equal(mlaw2lin(lin2mlaw(i_zero)), i_zero)
+    assert equal(mlaw2lin(lin2mlaw(i_max)),  i_max_gt)
