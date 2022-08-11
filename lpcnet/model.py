2bf09e4a27a910c8c8e3e3b87353b57fddc121eb
tarepan <tarepan5884@gmail.com>
2022-08-11 06:13:09 +0000

    Add teacher-forcing validation

--- lpcnet/model.py
+++ lpcnet/model.py
@@ -5,19 +5,21 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import Optional
 
+import numpy as np
 import torch
-from torch import nn, log, permute
+from torch import nn, exp, permute, amax, amin
+from torch.distributions.categorical import Categorical
 from torch.optim import Adam
 from torch.optim.lr_scheduler import LambdaLR
 import pytorch_lightning as pl
 from omegaconf import MISSING
-
+import soundfile as sf
 
 from .domain import FPitchCoeffSt1nStcBatch
 from .data.domain import St1SeriesNoisyDatum
 from .data.transform import ConfTransform, augment, collate, load_raw, preprocess
 from .networks.network import Network, ConfNetwork
-from .networks.components.mulaw import lin2mlawpcm, s16pcm_to_unit
+from .networks.components.mulaw import lin2mlawpcm, mlaw2lin, s16pcm_to_unit
 
 
 @dataclass
@@ -87,6 +89,8 @@ class Model(pl.LightningModule):
 
         # Forward :: ... -> ((B, T=t_s, JDist), (B, T=t_s,))
         e_t_mlaw_logp_series_estim, p_t_noisy_series = self.net(feat_series, pitch_series, lpcoeff_series, s_t_1_noisy_series, stateful=False)
+        s_t_series_fwd_estim = p_t_noisy_series + mlaw2lin(Categorical(exp(e_t_mlaw_logp_series_estim)).sample())
+        s_t_series_fwd_estim_unit = s16pcm_to_unit(s_t_series_fwd_estim)
 
         # Loss
         e_t_mlaw_series_ideal = lin2mlawpcm(s_t_clean_series - p_t_noisy_series)
@@ -100,7 +104,8 @@ class Model(pl.LightningModule):
         # Logging
         # [PyTorch](https://pytorch.org/docs/stable/tensorboard.html#torch.utils.tensorboard.writer.SummaryWriter.add_audio)
         #                                                      ::Tensor(1, L)
-        self.logger.experiment.add_audio(f"audio_{batch_idx}", s_t_series_estim_unit, global_step=self.global_step, sample_rate=self._conf.sampling_rate) # type: ignore
+        self.logger.experiment.add_audio(f"tf_{batch_idx}", s_t_series_fwd_estim_unit, global_step=self.global_step, sample_rate=self._conf.sampling_rate) # type: ignore
+        self.logger.experiment.add_audio(f"ar_{batch_idx}", s_t_series_estim_unit,     global_step=self.global_step, sample_rate=self._conf.sampling_rate) # type: ignore
 
         return {"val_loss": loss_fwd}
 
